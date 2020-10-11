0ff44ace8b33a01b6fd2ebee326ca18bf2f4a1a4
plutonium <artiukh.va@phystech.edu>
2020-10-11 22:49:56 +0300

    add task 7

---
+++ w4/7.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime
+from functools import wraps
+
+def logger(file):
+	def dec(f):
+		@wraps(f)
+		def wrapper(*args, **kwargs):
+			start = time.time()
+			f(*args, **kwargs)
+			stop = time.time()
+			timeinwork = stop - start
+			start, stop = datetime.fromtimestamp(start), datetime.fromtimestamp(stop)
+			returned = f(*args, **kwargs) if f(*args, **kwargs) is not None else '-'
+
+			with open(file, "w") as log:
+				log.write(
+				"called at {}\narguments: {}\nreturned: {}\nended at: {}\nexecution time:{}\n".format(start, args, returned, stop, timeinwork))
+
+			return f(*args, **kwargs)
+		return wrapper
+	return dec
+
+@logger("log.txt")
+def func(a, b):
+        return a + b
+
+func(1, 2)
