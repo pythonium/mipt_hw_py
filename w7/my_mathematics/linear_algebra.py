5af5741cffcfe431d541aa5e8f15d864248dc22c
plutonium <artiukh.va@phystech.edu>
2020-11-02 17:33:04 +0300

    update linear_algebra

--- w7/my_mathematics/linear_algebra.py
+++ w7/my_mathematics/linear_algebra.py
@@ -2,28 +2,27 @@ import math
 
 class Vector:
 
-    def __init__(self, inpstr):
-        inp = list(map(float, inpstr.split(',')))
+    def __init__(self, x, y, z):
 
-        self.x = inp[0]
-        self.y = inp[1]
-        self.z = inp[2]
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __str__(self):
         return f'({self.x}, {self.y}, {self.z})'
 
     def __add__(self, other):
-        return Vector(f'{self.x + other.x}, {self.y + other.y}, {self.z + other.z}')
+        return Vector({self.x + other.x}, {self.y + other.y}, {self.z + other.z})
 
     def __sub__(self, other):
-        return Vector(f'{self.x - other.x}, {self.y - other.y}, {self.z - other.z}')
+        return Vector({self.x - other.x}, {self.y - other.y}, {self.z - other.z})
 
     def __and__(self, other):
-        return Vector(f'{self.y*other.z - other.y*self.z}, {other.x*self.z - self.x*other.z}, {self.x*other.y - self.y*other.x}')
+        return Vector({self.y*other.z - other.y*self.z}, {other.x*self.z - self.x*other.z}, {self.x*other.y - self.y*other.x})
 
     def distance(self):
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
-
+'''
 x = Vector('1,2,3')
 y = Vector('2,3,4')
 print(x+y, x-y, x&y, sep = '\n')
@@ -44,3 +43,4 @@ for i in range(N):
 
 
 print(vectors[most_remote_vector])
+'''
