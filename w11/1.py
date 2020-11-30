65103ca8e443792c706db5746789a51170ebea8f
plutonium <artiukh.va@phystech.edu>
2020-11-30 10:23:53 +0300

    add 1.py

---
+++ w11/1.py
@@ -0,0 +1,19 @@
+import aiohttp
+import asyncio
+
+async def get_response(url):
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+
+            html = await response.text()
+            return html
+
+async def send_request(n = 10):
+    responses = [await get_response('http://127.0.0.1:8000') for i in range(n)]
+    print(*responses)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(send_request(10))
+loop.close()
