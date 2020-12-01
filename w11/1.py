b2d8032feaf2022ac40f3cec2f212cc2e6d17fbb
plutonium <artiukh.va@phystech.edu>
2020-12-01 15:13:01 +0300

    fix 1.py

--- w11/1.py
+++ w11/1.py
@@ -12,7 +12,7 @@ async def get_response(url):
 
 
 loop = asyncio.get_event_loop()
-n = int(input())
+n = 100
 responses = [get_response('http://127.0.0.1:8000') for _ in range(n)]
 print(*loop.run_until_complete(asyncio.gather(*responses)))
 loop.close()
