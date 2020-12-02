233ca9de5de54140527cb5215a8aec4bcb06aabc
plutonium <artiukh.va@phystech.edu>
2020-12-02 14:20:57 +0300

    done 3.py

--- w11/3.py
+++ w11/3.py
@@ -8,21 +8,29 @@ async def fetch(url):
         async with session.get(url) as response:
 
             html = await response.text()
-            html = re.split(r'[\n]', html)
-            async with aiofile.async_open("found.txt", 'w+') as found:
-                for line in lines:
-                    if line.startswith("<a >"):
-                        found.writer(line)
+            return html
 
 
-async def find_line(urls):
+async def find_line(text):
+    lines = text.split(r'\n')
+    async with aiofile.AIOFile("found.txt", 'w') as found:
+        for line in lines:
+            if line.startswith("<a >"):
+                await found.write(line)
+
+async def seeker(file):
+    async with aiofile.AIOFile(file, 'r') as urls:
+        htmls = []
+        async for url in aiofile.LineReader(urls):
+            htmls.append(fetch(url))
+        #есть массив со всеми текстами страниц
+        await asyncio.gather(*htmls)
 #    async with aiofile.async_open(urls, 'r') as urlslist:
-    async for line in aiofile.LineReader(urls):
-        asyncio.ensure_future(fetch(line))
+
 
 
 if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
-    with open('urls.txt', 'r') as urls:
-        loop.run_until_complete(find_line(urls))
+    loop.run_until_complete(seeker("urls.txt"))
+    loop.close()
